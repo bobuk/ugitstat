f8bb54bc75968f72aa113833ac0fa56c98f16792
Grigory Bakunov <thebobuk@ya.ru>
2015-09-18 17:19:09 +0300

    add config to server

--- server.py
+++ server.py
@@ -1,25 +1,40 @@
 import asyncio
+import subprocess
+import logging
+
+from configparser import ConfigParser
+
+C = ConfigParser()
+C.read('.ugitstat')
 
 class uGitServerProtocol:
+    def get_git_log(self):
+        git = subprocess.run(['git', 'log', '--oneline'], stdout=subprocess.PIPE)
+        return git.stdout.decode('utf-8').split(' ', 1)[0]
+
     def connection_made(self, transport):
         self.transport = transport
 
     def datagram_received(self, data, addr):
-        message = data.decode()
-        print('Received %r from %s' % (message, addr))
-        print('Send %r to %s' % (message, addr))
+        message = data.decode('utf-8')
+        data = 'GIT ' + self.get_git_log()
+        logging.debug('Received %r from %s' % (message, addr))
+        logging.debug('Send %r to %s' % ('GIT ' + self.get_git_log(), addr))
         self.transport.sendto(data, addr)
 
-loop = asyncio.get_event_loop()
-print("Starting uGitStat UDP server")
-listen = loop.create_datagram_endpoint(
-    uGitServerProtocol, local_addr=('0.0.0.0', 9798))
-transport, protocol = loop.run_until_complete(listen)
 
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    logging.info("Starting uGitStat UDP server")
+    listen = loop.create_datagram_endpoint(
+        uGitServerProtocol, local_addr=(C['server']['host'], 19798))
+    transport, protocol = loop.run_until_complete(listen)
+    logging.basicConfig(format='%(asctime)s %(message)s', level = C['server']['logging'])
 
-transport.close()
-loop.close()
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+
+    transport.close()
+    loop.close()
